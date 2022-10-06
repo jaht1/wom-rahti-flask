237808c7e1376bb0203745942bed44e699942ebe
jaht1 <ahtijenn@arcada.fi>
2022-10-06 19:46:53 +0000

    postgresql

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ load_dotenv()
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
     ret = { 
+        'method': request.method,
         'msg': 'Github webhook works', 
         'env': os.environ.get('ENV_VAR', 'Cannot find variable ENV_VAR') 
     }
